30ec057dd9fa52f3ce75d5a99e87f66618720879
zb1ackcat <douglass.dj@gmail.com>
2021-08-08 23:32:14 -0500

    before sc

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const dbHelper = require('./db.js')
+
+//
 const {Client, Pool} = require('pg')
 const util = require('util')
 
-let t = []
+
+
+let total = 81412
+let limit = 500
 let new_accounts = []
 let currupted = []
+let count = 0
 
 // Variables used to connect to the database. 
 
@@ -30,20 +35,20 @@ const new_db = new Pool({
 const db2 = old_db
 
 
-async function query_i(db1,ammount,offset){
-    for (let i = 0; i < ammount; i++){
-        db1.connect()
-        x = offset * i
-        await db1.query(` SELECT * FROM accounts OFFSET ${x} LIMIT ${offset}`)
+async function query_i(db1,total,limit){
+    for (let i = 0; i < total; i+ count){
+        client1 = await db1.connect()
+        x = limit * i
+        await db1.query(` SELECT * FROM accounts OFFSET ${x} LIMIT ${limit}`)
             .then(results => pull_comparision(results.rows))
-            //.then (results => console.log(results.rows))
+            .then (results => console.log(`This is the new ACCOUNT ${new_accounts.length} This is corruption ${currupted.length} ${currupted[0].name} ${currupted[0].currput_name}`))
             .catch(e => console.log(`THIS!!! ${e}`))
-            .finally (() => console.log(`This is the new ACCOUNT ${new_accounts.length} This is curuption ${currupted.length} ${currupted[0].name} ${currupted[0].currput_name}`))
+            .finally (() => client1.release())
     }
 
 }
 async function pull_comparision(results){
-
+    count = results.length
     for (let i = 0; i < results.length; i++){
         let rtc = {"id":results[i].id,"name":results[i].name,"email":results[i].email,"favorite_flavor":results[i].favorite_flavor}
 
@@ -54,15 +59,13 @@ async function compare_db(record){
     client = await db2.connect()
     await client.query(`SELECT * FROM accounts WHERE "id" = '${record.id}'`)
         .then( results => {
-            console.log(`STUFF!!! ${results.rows.length}`)
             if (results.rows.length  === 0 ){
                 new_accounts.push(record)   
             } else if (record.name != results.rows[0].name || record.email != results.rows[0].email) {
-                console.log(`record ${record.name } result ${results.rows[0].name} ${results.rows[0].email} ${results.rows}`)
                 arr = {"id":results.id,"name":record.rows,"email":record.email,"favorite_flavor":record.favorite_flavor,"currput_name":results.name,"curupt_email":results.email,"currupt_favorite_flavor":results.favorite_flavor}
                 currupted.push(arr)
             } else{
-                console.log(`Nothing`)
+                
             } })
         // .then( results => console.log(`THE RESULTS!!! ${JSON.stringify(results.rows)}`))
         .catch(e => console.log(`THAT!!! ${e}`))
@@ -73,6 +76,6 @@ async function compare_db(record){
 }
 
 
-query_i(new_db,1,10000)
+query_i(new_db,total,limit)
 
 // open_close(old_db)
